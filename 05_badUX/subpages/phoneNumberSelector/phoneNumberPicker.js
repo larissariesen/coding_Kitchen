423ca7f81a363fd7b39dea3b442850c1b23db7f2
larissa.riesen <larissa.riesen@hotmail.com>
2024-04-02 08:56:07 +0200

    feat: add buttons for in/decrease, validate phoneNr

--- 05_badUX/subpages/phoneNumberSelector/phoneNumberPicker.js
+++ 05_badUX/subpages/phoneNumberSelector/phoneNumberPicker.js
@@ -4,6 +4,32 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault();
     });
     input.addEventListener('change', (event) => {
-       // Todo - validate phone number
+        const phoneNumber = event.target.value;
+        const swissPhoneNumberPattern = /^(?:\s*\d\s*){9}$/;
+
+        if (swissPhoneNumberPattern.test(phoneNumber)) {
+            input.classList.remove('invalid');
+            input.classList.add('valid');
+        } else {
+            input.classList.remove('valid');
+            input.classList.add('invalid');
+        }
     });
 });
+
+const increase = () => {
+    const input = document.getElementById('phoneNr');
+    const value = parseInt(input.value) ? parseInt(input.value) : 0;
+    input.value = value + 1;
+    input.innerText = String(value + 1);
+    input.dispatchEvent(new Event('change'));
+};
+
+const decrease = () => {
+    const input = document.getElementById('phoneNr');
+    const value = parseInt(input.value) ? parseInt(input.value) : 0;
+    if(value === 0) return;
+    input.value = value - 1;
+    input.innerText = String(value - 1);
+    input.dispatchEvent(new Event('change'));
+};
