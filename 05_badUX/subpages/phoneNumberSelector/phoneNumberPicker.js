0991b77b470e898eda8d1ec3f976df26a1c4f325
larissa.riesen <larissa.riesen@hotmail.com>
2024-04-04 17:21:30 +0200

    feat: switch in/decrease btns

--- 05_badUX/subpages/phoneNumberSelector/phoneNumberPicker.js
+++ 05_badUX/subpages/phoneNumberSelector/phoneNumberPicker.js
@@ -18,18 +18,45 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const increase = () => {
+    const inputGroup = document.getElementsByClassName('input-group')[0];
     const input = document.getElementById('phoneNr');
+    const increaseBtn = document.getElementById('increaseButton');
+    const decreaseBtn = document.getElementById('decreaseButton');
     const value = parseInt(input.value) ? parseInt(input.value) : 0;
-    input.value = value + 1;
-    input.innerText = String(value + 1);
+    const newVal = value + 1;
+    input.value = newVal;
+    input.innerText = String(newVal);
+    inputGroup.innerHTML = '';
+    if(newVal%2 === 1) {
+        inputGroup.appendChild(decreaseBtn)
+        inputGroup.appendChild(input)
+        inputGroup.appendChild(increaseBtn)
+    } else {
+        inputGroup.appendChild(increaseBtn)
+        inputGroup.appendChild(input)
+        inputGroup.appendChild(decreaseBtn)
+    }
     input.dispatchEvent(new Event('change'));
 };
 
 const decrease = () => {
+    const inputGroup = document.getElementsByClassName('input-group')[0];
     const input = document.getElementById('phoneNr');
+    const increaseBtn = document.getElementById('increaseButton');
+    const decreaseBtn = document.getElementById('decreaseButton');
     const value = parseInt(input.value) ? parseInt(input.value) : 0;
+    const newVal = value - 1;
     if(value === 0) return;
-    input.value = value - 1;
-    input.innerText = String(value - 1);
+    input.value = newVal;
+    input.innerText = String(newVal);
+    if(newVal%2 === 0) {
+        inputGroup.appendChild(decreaseBtn)
+        inputGroup.appendChild(input)
+        inputGroup.appendChild(increaseBtn)
+    } else {
+        inputGroup.appendChild(increaseBtn)
+        inputGroup.appendChild(input)
+        inputGroup.appendChild(decreaseBtn)
+    }
     input.dispatchEvent(new Event('change'));
 };
