a53e357495175344eb96058184fa035cf3718cb1
larissa.riesen <larissa.riesen@hotmail.com>
2024-05-03 16:52:06 +0200

    feat: 2nd try - using opacity - broke UI - trying to use grid

--- 06_tabbing/tabbing.js
+++ 06_tabbing/tabbing.js
@@ -1,17 +1,22 @@
-function openCity(cityName, el, color) {
+function openTab(cityName, el, color) {
     let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.opacity = "0";
+        if (tabcontent[i].id === cityName) {
+            tabcontent[i].style.opacity = "1"; // Set opacity to 1 for selected tab content
+            tabcontent[i].style.display = "block"; // Show selected tab content
+        } else {
+            tabcontent[i].style.opacity = "0"; // Set opacity to 0 for other tab contents
+            tabcontent[i].style.display = "none"; // Hide other tab contents
+        }
     }
 
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.backgroundColor = ""; // Reset background color of all tab links
     }
 
-    document.getElementById(cityName).style.opacity = "1";
-    el.style.backgroundColor = color;
+    el.style.backgroundColor = color; // Set background color of selected tab link
 }
 
 window.onload = () => {
