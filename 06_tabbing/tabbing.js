660abbf884af073a7c238a4d3791e9321ad786ac
larissa.riesen <larissa.riesen@hotmail.com>
2024-05-06 19:04:35 +0200

    feat: use grid and fix so back to firstVersion but with grid

--- 06_tabbing/tabbing.js
+++ 06_tabbing/tabbing.js
@@ -1,22 +1,21 @@
-function openTab(cityName, el, color) {
+function openTab(id, el, color) {
     let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        if (tabcontent[i].id === cityName) {
-            tabcontent[i].style.opacity = "1"; // Set opacity to 1 for selected tab content
-            tabcontent[i].style.display = "block"; // Show selected tab content
+        if (tabcontent[i].id === id) {
+            tabcontent[i].style.display = "block";
+            tabcontent[i].style.background = color;
         } else {
-            tabcontent[i].style.opacity = "0"; // Set opacity to 0 for other tab contents
-            tabcontent[i].style.display = "none"; // Hide other tab contents
+            tabcontent[i].style.display = "none";
         }
     }
 
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = ""; // Reset background color of all tab links
+        tablinks[i].style.backgroundColor = "";
     }
 
-    el.style.backgroundColor = color; // Set background color of selected tab link
+    el.style.backgroundColor = color;
 }
 
 window.onload = () => {
